574c83c8a04c63ab235649872ae848d7b8b96d28
mgpacifque <ovouzzy@gmail.com>
2025-10-24 17:35:25 +0200

    Fix loop range and improve number formatting in 5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i < 99:
-        print("{:02}".format(i), end=", ")
-    else:
-        print("{:02}".format(i))
+for i in range(99):
+    print("{:02d}".format(i), end=", " if i < 99 else "\n")
